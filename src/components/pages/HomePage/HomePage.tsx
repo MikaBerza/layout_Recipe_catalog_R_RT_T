7010bf02469999ad82635d1074942929a4b09aa0
Mika <mikberza@mail.ru>
2023-12-17 16:08:52 +0300

    Change (components): changing a component #1 #1 -the internal container has been removed from the component (HomePage.tsx), styles have been added to the subtitle -added styles to the component (MainTitle.tsx)
    Изменять (components): изменение компонента #1
    #1
    -из компонента (HomePage.tsx) убран внутренний контейнер, добавлены стили подзаголовку
    -добавлены стили компоненту (MainTitle.tsx)

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -5,16 +5,11 @@ import styles from './HomePage.module.css';
 const HomePage = () => {
   return (
     <main className={styles.wrapper}>
-      <div className={styles.inner}>
-        <MainTitle textTitle='Ведите свой кулинарный каталог онлайн и делитесь им с друзьями' />
-        <p className={styles.subtitle}>
-          Создавайте, сохраняйте и редактируйте свои персональные рецепты
-        </p>
-        <ButtonCreateCatalog
-          name='Создать каталог'
-          link='/recipe-catalog-page'
-        />
-      </div>
+      <MainTitle textTitle='Ведите свой кулинарный каталог онлайн и делитесь им с друзьями' />
+      <p className={styles.subtitle}>
+        Создавайте, сохраняйте и редактируйте свои персональные рецепты
+      </p>
+      <ButtonCreateCatalog name='Создать каталог' link='/recipe-catalog-page' />
     </main>
   );
 };
