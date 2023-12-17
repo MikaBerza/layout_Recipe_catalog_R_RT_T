4f99f37a349035f3e2a39967699e164d3c498aad
Mika <mikberza@mail.ru>
2023-12-17 16:52:21 +0300

    Remove (src): unnecessary elements #1 #1 -removed unnecessary elements from the file (App.tsx) -added re-export (RecipeCatalogPage) to file (index.tsx)
    Удалить (src): ненужные элементы #1
    #1
    -удалены лишние элементы из файла (App.tsx)
    -добавлен реэкспорт (RecipeCatalogPage) в файл (index.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/commons/Header/Header';
-import { HomePage } from './components/pages/index';
+import { HomePage, RecipeCatalogPage } from './components/pages/index';
 import Footer from './components/commons/Footer/Footer';
-import RecipeCatalogPage from './components/pages/RecipeCatalogPage/RecipeCatalogPage';
 
 function App() {
   return (
-    <Router>
-      <>
-        <Header />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route
-            path='/recipe-catalog-page'
-            element={<RecipeCatalogPage />}
-          />
-        </Routes>
-        <Footer />
-      </>
-    </Router>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/recipe-catalog-page' element={<RecipeCatalogPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
